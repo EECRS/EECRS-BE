0dc41feabb9ca2d32311148c82dbb95afbfc9527
mike <mikesol136@gmail.com>
2020-03-20 22:36:43 -0400

    add location atribute

--- src/main/java/com/eecrs/eecrsbe/entity/Location.java
+++ src/main/java/com/eecrs/eecrsbe/entity/Location.java
@@ -1,4 +1,21 @@
 package com.eecrs.eecrsbe.entity;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Data
 public class Location {
+
+    @ManyToOne
+    @JoinColumn(name = "woreda")
+    private Woreda woreda;
+
+    @Column
+    private String latitude;
+
+    @Column
+    private String longitude;
 }
